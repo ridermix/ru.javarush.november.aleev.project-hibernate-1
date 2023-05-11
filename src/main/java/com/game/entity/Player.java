388702f10dba4e4ed42d39b6f0235cf100332441
rustekapl <rustekapl@gmail.com>
2023-05-11 10:19:14 +0200

    Tasks 1-9 completed

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,29 +1,38 @@
 package com.game.entity;
 
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
 @Table(name = "player", schema = "rpg")
 public class Player {
-    @Column(name = "id", nullable = false)
+
+    @Id
+    @Column(nullable = false)
     private Long id;
+
     @Column(name = "name", length = 12, nullable = false)
     private String name;
+
     @Column(name = "title", length = 30, nullable = false)
     private String title;
+
+    @Column(name = "race", nullable = false)
     @Enumerated(EnumType.ORDINAL)
     private Race race;
+
+    @Column(name = "profession", nullable = false)
     @Enumerated(EnumType.ORDINAL)
     private Profession profession;
 
+    @Column(name = "birthday", nullable = false)
     private Date birthday;
 
+    @Column(name = "banned", nullable = false)
     private Boolean banned;
 
+    @Column(name = "level", nullable = false)
     private Integer level;
 
     public Player() {
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,22 +1,46 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+//import com.mysql.cj.xdevapi.SessionFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
-
 import javax.annotation.PreDestroy;
+import java.util.Properties;
 import java.util.List;
 import java.util.Optional;
 
+
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
+    private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "qwerty");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
 
+        sessionFactory = (SessionFactory) new Configuration()
+                .addAnnotatedClass(Player.class)
+                .addProperties(properties)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        try (Session session = sessionFactory.openSession()){
+            NativeQuery<Player> query = session.createNativeQuery("select * from rpg.player", Player.class);
+            query.setFirstResult(pageNumber * pageSize);
+            query.setMaxResults(pageSize);
+            return query.list();
+        }
     }
 
     @Override
